d9d46ed9a764210a08b15f97ba8ec7289f05b217
Jason O'Neil <jason.oneil@gmail.com>
2013-10-30 13:20:11 +0800

    Added `Random.shuffle( array );`

--- src/Random.hx
+++ src/Random.hx
@@ -53,10 +53,26 @@ class Random
 		return (arr != null && arr.length > 0) ? arr[int(0, arr.length - 1)] : null;
 	}
 
+	/** Shuffle an Array.  This operation affects the array in place, and returns that array.
+		The shuffle algorithm used is a variation of the [Fisher Yates Shuffle](http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) */
+	public static function shuffle<T>(arr:Array<T>):Array<T>
+	{
+		if (arr!=null) {
+			for (i in 0...arr.length) {
+				var j = int(0, arr.length - 1);
+				var a = arr[i];
+				var b = arr[j];
+				arr[i] = b;
+				arr[j] = a;
+			}
+		}
+		return arr;
+	}
+
 	/** Return a random item from an iterable.  Will return null if the iterable is null or empty. */
 	public static inline function fromIterable<T>(it:Iterable<T>):Null<T>
 	{
-		return (arr != null) ? fromArray(Lambda.array(it)) : null;
+		return (it != null) ? fromArray(Lambda.array(it)) : null;
 	}
 
 	/** Return a random constructor from an Enum.  Will return null if the enum has no constructors. Only works with enum constructors that take no parameters. */
