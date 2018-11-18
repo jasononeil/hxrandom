42c0ad082cc316d9fcd9c306d4069e78e9468c1e
Joseph Cloutier <jkcloutier@gmail.com>
2018-11-18 01:49:38 -0500

    Use the actual Fisher-Yates shuffle.

--- src/Random.hx
+++ src/Random.hx
@@ -54,16 +54,15 @@ class Random
 	}
 
 	/** Shuffle an Array.  This operation affects the array in place, and returns that array.
-		The shuffle algorithm used is a variation of the [Fisher Yates Shuffle](http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) */
+		The shuffle algorithm used is the [Fisher-Yates shuffle](http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) */
 	public static function shuffle<T>(arr:Array<T>):Array<T>
 	{
 		if (arr!=null) {
-			for (i in 0...arr.length) {
-				var j = int(0, arr.length - 1);
-				var a = arr[i];
-				var b = arr[j];
-				arr[i] = b;
-				arr[j] = a;
+			for (i in 0...(arr.length - 1)) {
+				var j = int(i, arr.length - 1);
+				var temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
 			}
 		}
 		return arr;
